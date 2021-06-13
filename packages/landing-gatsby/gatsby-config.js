184202f3e7d30dabc0883e5b2094e35039cc00b1
Manos Pappas <mano.pappa@gmail.com>
2021-06-13 21:00:35 +0100

    Update readme (#6)

--- packages/landing-gatsby/gatsby-config.js
+++ packages/landing-gatsby/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Full Stack Software Engineer`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A Greece born, London-based software engineer who is passionate about Web technologies and Cloud communications.`,
+    author: `@manospappas`,
   },
   plugins: [
     {
